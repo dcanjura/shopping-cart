9c41f1e8f4b01301ae1b1fbca67da644b616bc69
dcanjura <diegoac053@gmail.com>
2026-05-05 17:58:24 -0600

    feat: Initial configuration for CartService application, endpoints and business logic added

---
+++ cart-service/src/main/java/controller/CartController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import dto.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import service.CartService;
+
+@RestController
+@RequestMapping("/api/cart")
+@Validated
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService service;
+
+    /**
+     * Create the shopping cart using userId from request body
+     * @param request
+     * @return CartResponse
+     */
+    @PostMapping("/create")
+    public ResponseEntity<CartResponse> createCart(@RequestBody @Validated CreateCartRequest request){
+        return ResponseEntity.ok(service.create(request));
+    }
+
+    /**
+     * Get the cart info using userId from request
+     * @param request
+     * @return CartResponse
+     */
+    @GetMapping("/getCartByUser")
+    public ResponseEntity<CartResponse> getByUserId(@RequestBody @Validated CreateCartRequest request){
+        return ResponseEntity.ok(service.getByUser(request));
+    }
+
+    @PostMapping("/addItem/{cartId}")
+    public ResponseEntity<CartResponse> addItem(@RequestBody @Validated AddItemRequest request, @PathVariable Long cartId){
+        return ResponseEntity.ok(service.addItem(request, cartId));
+    }
+
+    @PutMapping("/updateItem/{cartId}/{productId}")
+    public ResponseEntity<CartResponse> updateItem(@RequestBody @Validated UpdateItemRequest request, @PathVariable @Validated Long cartId, @PathVariable @Validated String productId){
+        return ResponseEntity.ok(service.updateItem(request, cartId, productId));
+    }
+
+    @DeleteMapping("/removeItem/{cartId}/{productId}")
+    public ResponseEntity<CartResponse> removeItem(@PathVariable @Validated Long cartId, @PathVariable @Validated String productId){
+        return ResponseEntity.ok(service.removeItem(cartId, productId));
+    }
+}
