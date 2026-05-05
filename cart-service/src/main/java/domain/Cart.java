93d55b872f6421a99844baf675acd483fb2db3b9
dcanjura <diegoac053@gmail.com>
2026-05-05 13:47:32 -0600

    feat: Entities added to domain package

---
+++ cart-service/src/main/java/domain/Cart.java
@@ -0,0 +1,28 @@
+package domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "Cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Item> items = new ArrayList<>();
+
+    private Double total;
+}
