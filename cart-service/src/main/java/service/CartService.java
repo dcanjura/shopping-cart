8d730ab832d0ebab83c6e02d7b9aa185e7b91bda
dcanjura <diegoac053@gmail.com>
2026-05-05 20:31:25 -0600

    feat: CartService updated to communicate with product-service

--- cart-service/src/main/java/service/CartService.java
+++ cart-service/src/main/java/service/CartService.java
@@ -1,5 +1,6 @@
 package service;
 
+import client.ProductClient;
 import domain.Cart;
 import domain.Item;
 import dto.*;
@@ -16,6 +17,7 @@ import java.util.Optional;
 public class CartService {
 
     private final CartRepository repository;
+    private final ProductClient productClient;
 
     public CartResponse create(CreateCartRequest request){
         return CartMapper.toResponse(repository.createCart(request.userId()));
@@ -36,10 +38,11 @@ public class CartService {
                     i.setQuantity(i.getQuantity() + request.quantity());
                 },
                 () -> {
+                    ProductResponse product = productClient.getProductById(request.productId());
                     Item newItem = Item.builder()
                             .productId(request.productId())
                             .quantity(request.quantity())
-                            .price(20.00)
+                            .price(product.price())
                             .cart(cart)
                             .build();
 
