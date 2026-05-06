f14f125ac6aaaac5e2cd945f85d236d1dcd99431
dcanjura <diegoac053@gmail.com>
2026-05-06 08:29:48 -0600

    feat: Implementation for API communication added

--- order-service/src/main/java/controller/OrderController.java
+++ order-service/src/main/java/controller/OrderController.java
@@ -8,14 +8,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.OrderService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/orders")
 public class OrderController {
 
+    private final OrderService service;
+
     @PostMapping("/save")
     public ResponseEntity<OrderResponse> createOrder(@PathVariable @Validated String id){
-        return ResponseEntity.ok();
+        return ResponseEntity.ok(service.createOrder(id));
     }
 }
